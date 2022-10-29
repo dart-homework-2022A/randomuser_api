66e2b1e2ff89115a10fe2e40a5cb92cae8a36b1a
ZARIFMAC <uzbzarif@gmail.com>
2022-10-29 15:22:35 +0500

    URI from string

--- bin/randomuser_api.dart
+++ bin/randomuser_api.dart
@@ -1,1 +1,14 @@
-void main() {}
+import 'package:http/http.dart' as http;
+
+void main() {
+  Uri uri = Uri.parse('https://randomuser.me/api/');
+  print(uri.scheme);
+  // The path component.
+  print(uri.path);
+  // Set the query parameters.
+  uri = uri.replace(queryParameters: {'results': '10'});
+
+  // The host component.
+  print(uri.host);
+  print(uri);
+}
